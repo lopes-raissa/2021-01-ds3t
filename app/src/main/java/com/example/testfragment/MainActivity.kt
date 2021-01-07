30363418a29e091be36567d0a162566d956f6790
Raissa Lopes <raissasan6@gmail.com>
2021-01-07 16:56:56 -0300

    Primeira Versão

---
+++ app/src/main/java/com/example/testfragment/MainActivity.kt
@@ -0,0 +1,87 @@
+package com.example.testfragment
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.MenuItem
+import android.view.View
+import android.widget.Button
+import com.example.testfragment.fragments.ConsoleFragment
+import com.example.testfragment.fragments.GameFragment
+import com.example.testfragment.fragments.HomeFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity(), View.OnClickListener, BottomNavigationView. OnNavigationItemSelectedListener {
+
+    private lateinit var buttonHome: Button
+    private lateinit var buttonConsoles: Button
+    private lateinit var buttonGames: Button
+
+    private lateinit var homeFragment: HomeFragment
+    private lateinit var gameFragment: GameFragment
+    private lateinit var consoleFragment: ConsoleFragment
+
+    private lateinit var bottomNavigation: BottomNavigationView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        buttonHome = findViewById(R.id.button_home)
+        buttonHome.setOnClickListener(this)
+
+        buttonConsoles = findViewById(R.id.button_consoles)
+        buttonConsoles.setOnClickListener(this)
+
+        buttonGames = findViewById(R.id.button_games)
+        buttonGames.setOnClickListener(this)
+
+        homeFragment = HomeFragment()
+        consoleFragment = ConsoleFragment()
+        gameFragment = GameFragment()
+
+        bottomNavigation = findViewById(R.id.bottom_navigation)
+        bottomNavigation.setOnNavigationItemSelectedListener(this)
+
+        supportFragmentManager.beginTransaction().replace(R.id.frame, gameFragment).commit()
+    }
+
+    override fun onClick(v: View) {
+        when (v.id) {
+            R.id.button_home -> {
+                supportFragmentManager.beginTransaction().replace(R.id.frame, homeFragment).commit()
+
+            }
+            R.id.button_consoles -> {
+                supportFragmentManager.beginTransaction().replace(R.id.frame, consoleFragment).commit()
+
+            }
+            R.id.button_games -> {
+                supportFragmentManager.beginTransaction().replace(R.id.frame, gameFragment).commit()
+            }
+        }
+    }
+
+
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+
+
+        when (item.itemId) {
+            R.id.menu_home -> {
+                supportFragmentManager.beginTransaction().replace(R.id.frame, homeFragment).commit()
+            }
+             R.id.menu_consoles -> {
+                 supportFragmentManager.beginTransaction().replace(R.id.frame, consoleFragment).commit()
+             }
+
+            R.id.menu_games -> {
+                supportFragmentManager.beginTransaction().replace(R.id.frame, gameFragment).commit()
+            }
+        }
+
+
+        return true
+    }
+
+
+}
